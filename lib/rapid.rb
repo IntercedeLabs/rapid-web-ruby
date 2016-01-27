159b7f3317a729f4f50311c421a0fab9f9967d32
Ben Arnold <ben.arnold@intercede.com>
2016-01-27 10:39:34 +0000

    Connects to server successfully

--- lib/rapid.rb
+++ lib/rapid.rb
@@ -1,8 +1,42 @@
 require "rapid/version"
+require 'jwt'
+require 'RestClient'
+require 'json'
 
 class Rapid
 
-  def initialize
-  
+  attr_accessor :rapid_request_url
+  attr_reader :rapid_client_key
+
+  def initialize(host, rapid_client_key)
+    raise ArgumentError, 'Missing Rapid host' unless host
+    @rapid_request_url = "https://#{host}/1.1/RequestCredential"
+    @rapid_client_key = rapid_client_key
+  end
+
+  def request(subjectName)
+    payload = { :SubjectName => subjectName }
+    jwt = signRequest payload
+
+    begin
+        response = sendRequest jwt
+        return response["Identifier"]
+    rescue RestClient::Exception => e
+        begin
+            response = JSON.parse(e.response)
+            raise RuntimeError, "Requesting credential failed with server message '#{response["Message"]}'"
+        rescue JSON::ParserError
+            raise RuntimeError, "Requesting credential failed with HTTP status '#{e.response.code}'"
+        end
+    end
+  end
+
+  def signRequest(payload)
+    return JWT.encode payload, rapid_client_key, 'RS256'
+  end
+
+  def sendRequest(request)
+    response = RestClient.post @rapid_request_url, request, :content_type => :json, :accept => :json
+    return JSON.parse(response)
   end
 end
--- test/rapid_test.rb
+++ test/rapid_test.rb
@@ -1,11 +1,32 @@
 require 'test_helper'
 
 class RapidTest < Minitest::Test
+
+  def test_rapid_connectrs_to_rapid_server_at_url
+    rapid = Rapid.new("rapid-demo.intercedelabs.com/rapid", load_key())
+    id = rapid.request("RapidSecurity.Test.User")
+    print "\n\nRequest ID: #{id}\n"
+  end
+
+
   def test_that_it_has_a_version_number
     refute_nil ::Rapid::VERSION
   end
 
-  def test_that_it_throws_if_constructed_from_null_url
-    refute_nil ::Rapid.new
+  def test_that_it_throws_if_constructed_from_nil_url
+    assert_raises ArgumentError do 
+        ::Rapid.new(nil, nil)
+    end 
   end
+
+  def test_that_rapid_pads_host_and_path_with_scheme_and_version
+    server = Rapid.new("server/rapid", nil)
+    assert_equal "https://server/rapid/1.1/RequestCredential", server.rapid_request_url
+  end
+
+  def load_key
+    p12 = OpenSSL::PKCS12.new(File.binread("rapid.testing.rp.pfx"), "rapidtests")
+    p12.key
+  end
+
 end
